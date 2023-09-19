4aa6faf276c8a89e20dc30ac96b64ef4baaaef4f
Aliaksei Yankouski <minnesotanhlwild@gmail.com>
2023-09-19 12:50:21 +0300

    burger add

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+const burger_button = document.querySelector('.burger-icon');
+const nav = document.querySelector('nav');
+const header_content = document.querySelector('.header-content');
+
+burger_button.onclick = () => hide(nav, header_content);
+
+function hide(el, el2) {
+   if (el.classList.contains('_active')) {
+      el.classList.remove('_active');
+      el2.classList.remove('_hide');
+      burger_button.classList.remove('close');
+   } else {
+      el.classList.add('_active');
+      el2.classList.add('_hide');
+      burger_button.classList.add('close');
+   }
+}
